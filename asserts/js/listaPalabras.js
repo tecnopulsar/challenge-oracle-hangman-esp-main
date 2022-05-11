4e470af94679fd197a5c925c84052d04dad29a4e
tecnopulsar <tecnopulsar@gmail.com>
2022-05-11 08:15:46 -0300

    test diccionario

--- asserts/js/listaPalabras.js
+++ asserts/js/listaPalabras.js
@@ -1,17 +1,40 @@
 var arrayPalabras = new Array();
-var archivoTxt = new XMLHttpRequest();
-var fileRuta = 'asserts/js/listaAnimales.txt';
-var txt;
+// var archivoTxt = new XMLHttpRequest();
+// var fileRuta = 'asserts/js/listaAnimales.txt';
+// var txt;
 
-/* Con el método .open() solo preparamos la petición, pero la haremos finalmente efectiva con el método .send(), el cuál lanzará la petición al servidor. Poco después, si consultamos el contenido de la propiedad .responseText (texto de respuesta) de nuestra instancia*/
+// /* Con el método .open() solo preparamos la petición, pero la haremos finalmente efectiva con el método .send(), el cuál lanzará la petición al servidor. Poco después, si consultamos el contenido de la propiedad .responseText (texto de respuesta) de nuestra instancia*/
 
-archivoTxt.addEventListener("readystatechange", () => {
-  if (archivoTxt.readyState === 4 && archivoTxt.status === 200){
-    txt= archivoTxt.responseText;
-    arrayPalabras = txt.split('\r\n');
-    console.log(arrayPalabras);
-  }
-});
+// archivoTxt.addEventListener("readystatechange", () => {
+//   if (archivoTxt.readyState === 4 && archivoTxt.status === 200){
+//     txt= archivoTxt.responseText;
+//     arrayPalabras = txt.split('\r\n');
+//     console.log(arrayPalabras);
+//   }
+// });
 
-archivoTxt.open("GET",fileRuta);
-archivoTxt.send();
+// archivoTxt.open("GET",fileRuta);
+// archivoTxt.send();
+
+arrayPalabras = [
+  'Abeja',     'Abejorro',  'Aguila',       'Alce',       'Anchoa',
+  'Anguila',   'Araña',     'Armadillo',    'Asno',       'Avestruz',
+  'Avispa',  'Ballena',   'Boa',          'Bufalo',     'Buho',
+  'Buitre',    'Burro',   'Caballo',      'Cacatua',    'Camello',
+  'Canguro',   'Canario',   'Cangrejo',     'Caracol',    'Castor',
+  'Cebra',     'Cerdo',     'Chacal',       'Chimpance',  'Ciervo',
+  'Cisne',     'Cocodrilo', 'Colibri',      'Conejo',     'Condor',
+  'Cotorra',   'Delfin',    'Dingo',        'Dromedario', 'Elefante',
+  'Emú',       'Equidna',   'Erizo',        'Escarabajo', 'Escorpión',
+  'Escuerzo',  'Faisán',    'Flamingo',     'Flamenco',   'Foca',
+  'Gacela',    'Galápago',  'Gallina',      'Gallipato',  'Gallo',
+  'Gamba',     'Ganso',     'Garcilla',     'Garrapata',  'Garza',
+  'Gato',      'Gavilán',   'Glotón',       'Golondrina', 'Gorila',
+  'Gorgojo',   'Gorrión',   'Grillo',       'Grulla',     'Guepardo',
+  'Halcón',    'Hamster',   'Hiena',        'Hipocampo',  'Hipopótamo',
+  'Holoturia', 'Hormiga',   'Hurón',        'Hupón',      'Ibice',
+  'Icotea',    'Iguana',    'Insecto palo', 'Intendio',   'Impala',
+  'Jabalí',    'Jabirú',    'Jerbo',        'Jirafa',     'Jaguar',
+  'Kaguang',   'Kiwi',      'Krill',        'Koala',    'Labro',
+  'Lagarto',   'Langosta',  'Langostino',   'Lechuza',    'Lémur'
+]
